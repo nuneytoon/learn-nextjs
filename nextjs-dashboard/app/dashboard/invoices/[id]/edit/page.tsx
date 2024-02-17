3d132e97caecf1abd6392af60c2aecd831405ba8
Matt Nunemacher <5334417+nuneytoon@users.noreply.github.com>
2024-02-17 17:58:35 +0000

    add lint script and use prettier for uniformity across the application

--- nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,12 +2,12 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
- 
+
 export default async function Page({ params }: { params: { id: string } }) {
   const { id } = params;
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
-    fetchCustomers(),    
+    fetchCustomers(),
   ]);
 
   if (!invoice) {
--- nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -6,14 +6,14 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
- 
+
 export default async function Page({
-    searchParams,
+  searchParams,
 }: {
-    searchParams?: {
-        query?: string;
-        page?: string;
-    };
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
 }) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
@@ -29,9 +29,9 @@ export default async function Page({
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-        <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-          <Table query={query} currentPage={currentPage} />
-        </Suspense>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
       <div className="mt-5 flex w-full justify-center">
         <Pagination totalPages={totalPages} />
       </div>
